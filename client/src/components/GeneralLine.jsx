59f2c4c1f1607ed899b6b9ef136701457f8a8d75
asaminiko <chebakova29@gmail.com>
2025-05-17 13:58:38 +0300

    general line

---
+++ client/src/components/GeneralLine.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const GeneralLine = () => {
+  //головна лінія
+  return (
+    <div className='frame3'>
+      <p className='formLogo'>ToDo</p>
+    </div>
+  )
+}
+
+export default GeneralLine
