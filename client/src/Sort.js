7d93cfcae3a536fc9e3d61ba18d6b0b81eba91a8
asaminiko <chebakova29@gmail.com>
2025-05-24 12:52:02 +0300

    sort by date

--- client/src/Sort.js
+++ client/src/Sort.js
@@ -26,3 +26,31 @@ export function binaryInsertionSortByPriority(arr) {
 
   return arr
 }
+//Використовується сортування вставкою з використанням бінарного пошуку.
+export function binaryInsertionSortByDate(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    const value = arr[i]
+    let start = 0
+    let end = i
+    const position = binarySearchByDate(arr, value, start, end)
+
+    for (let j = i; j > position; j--) {
+      arr[j] = arr[j - 1]
+    }
+
+    arr[position] = value
+  }
+
+  return arr
+}
+function binarySearchByDate(arr, value, start, end) {
+  while (start < end) {
+    const mid = Math.floor((start + end) / 2)
+    if (arr[mid].date < value.date) {
+      start = mid + 1
+    } else {
+      end = mid
+    }
+  }
+  return start
+}
